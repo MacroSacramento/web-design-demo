a4651386b6a9977303d0550190a77a9ec375b400
Marco Sarmiento <marco.sarmiento1120@gmail.com>
2025-01-30 02:31:43 -0700

    Remove unused dependencies and plugins from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,15 @@
 import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import wasm from 'vite-plugin-wasm';
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "/web-design-demo/",
     plugins: [
-        wasm(),
         react(),
         tailwindcss(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'node_modules/@libav.js/variant-default/dist/*.wasm.*',
-                    dest: 'assets/',
-                },
-            ],
-        }),
     ],
-    optimizeDeps: {
-        exclude: ['@libav.js/variant-default']
-    },
-    // build: {
-    //     rollupOptions: {
-    //         external: ['@libav.js/variant-default'],
-    //     }
-    // },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
